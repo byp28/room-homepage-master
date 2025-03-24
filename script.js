0099ee7555c3bb9e8c12ff85317f635563706c18
byp28 <yannight28@gmail.com>
2025-03-24 20:50:50 +0100

    3

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 const sliders = document.querySelectorAll(".slider")
 const desktop_sliders_img = document.querySelectorAll(".desktop-slider-img")
+const mobile_slider_img = document.querySelectorAll(".mobile-slider-img")
 let index = 0;
 const btn_next = document.querySelector('.btn-next');
 const btn_prev = document.querySelector('.btn-prev');
+const mobile_btn_next = document.querySelector('.mobile-btn-next');
+const mobile_btn_prev = document.querySelector('.mobile-btn-prev');
 
-btn_next.addEventListener("click",()=>{
+function next(){
     sliders[index].classList.add("hidden");
     desktop_sliders_img[index].classList.add("hidden");
+    mobile_slider_img[index].classList.add("hidden");
     if(index>1){
         index = 0;
         sliders[index].classList.remove("hidden");
         desktop_sliders_img[index].classList.remove("hidden");
+        mobile_slider_img[index].classList.remove("hidden");
     }else{
         index+=1;
         sliders[index].classList.remove("hidden");
         desktop_sliders_img[index].classList.remove("hidden");
+        mobile_slider_img[index].classList.remove("hidden");
     }
-})
+}
 
-btn_prev.addEventListener("click",()=>{
+function previous(){
     sliders[index].classList.add("hidden");
     desktop_sliders_img[index].classList.add("hidden");
+    mobile_slider_img[index].classList.add("hidden");
     if(index<1){
         index = sliders.length-1;
         sliders[index].classList.remove("hidden");
         desktop_sliders_img[index].classList.remove("hidden");
+        mobile_slider_img[index].classList.remove("hidden");
     }else{
         index-=1;
         sliders[index].classList.remove("hidden");
         desktop_sliders_img[index].classList.remove("hidden");
+        mobile_slider_img[index].classList.remove("hidden");
     }
-})
+}
+
+btn_next.addEventListener("click",()=> next())
+btn_prev.addEventListener("click",()=> previous())
+
+mobile_btn_next.addEventListener("click",()=> next())
+mobile_btn_prev.addEventListener("click",()=> previous())
